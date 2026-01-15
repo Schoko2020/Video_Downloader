4d2368fe4ddfcc9a794834229a0756935fdfb2ca
Eric <eric2007ms@gamil.com>
2026-01-15 20:13:17 +0100

    Anpassungen

--- logic.py
+++ logic.py
@@ -1,6 +1,12 @@
 import yt_dlp
+import os
 
 def run_download(link, progress_func):
+
+    save_path = os.path.join(os.path.expanduser("~"), "Videos")
+
+    target_file = os.path.join(save_path, '%(title)s.%(ext)s')
+
     ydl_opts = {
         'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
         'outtmpl': '%(title)s.%(ext)s',
